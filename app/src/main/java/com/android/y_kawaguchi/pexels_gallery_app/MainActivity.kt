a0597808c65253ea8d085db583585f76146b0fa9
plmnbhuiokj <plmnbhuiokj@gmail.com>
2021-09-22 19:24:49 +0900

    Initial commit

---
+++ app/src/main/java/com/android/y_kawaguchi/pexels_gallery_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.android.y_kawaguchi.pexels_gallery_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
